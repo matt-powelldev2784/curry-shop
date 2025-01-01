0e4dc9e35bad98dfa860a58dfa6969a51020b1af
Matthew Powell <matt.powell2784@gmail.com>
2025-01-01 14:04:17 +0000

    fix: export menu page as default export

--- app/pages/menu/page.tsx
+++ app/pages/menu/page.tsx
@@ -6,33 +6,33 @@ const MENU_ITEMS_QUERY = `*[_type == "menuItems"]{ _id, name, description, price
 
 const options = { next: { revalidate: 30 } }
 
-export const Menu = async () => {
-  const menuItems = await sanityClient.fetch<SanityDocument[]>(
-    MENU_ITEMS_QUERY,
-    {},
-    options
-  )
+ const Menu = async () => {
+   const menuItems = await sanityClient.fetch<SanityDocument[]>(
+     MENU_ITEMS_QUERY,
+     {},
+     options
+   )
 
-  return (
-    <main className="container mx-auto min-h-screen max-w-3xl p-8">
-      <h1 className="text-4xl font-bold mb-8">Menu Items</h1>
-      <ul className="flex flex-col gap-y-4">
-        {menuItems.map((menuItem) => (
-          <li className="hover:underline" key={menuItem._id}>
-            <Link href={`/pages/menu/${menuItem.slug.current}`}>
-              <h2 className="text-xl font-semibold">{menuItem.name}</h2>
-              <p>
-                {menuItem.price.toLocaleString('en-GB', {
-                  style: 'currency',
-                  currency: 'GBP',
-                })}
-              </p>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </main>
-  )
-}
+   return (
+     <main className="container mx-auto min-h-screen max-w-3xl p-8">
+       <h1 className="text-4xl font-bold mb-8">Menu Items</h1>
+       <ul className="flex flex-col gap-y-4">
+         {menuItems.map((menuItem) => (
+           <li className="hover:underline" key={menuItem._id}>
+             <Link href={`/pages/menu/${menuItem.slug.current}`}>
+               <h2 className="text-xl font-semibold">{menuItem.name}</h2>
+               <p>
+                 {menuItem.price.toLocaleString('en-GB', {
+                   style: 'currency',
+                   currency: 'GBP',
+                 })}
+               </p>
+             </Link>
+           </li>
+         ))}
+       </ul>
+     </main>
+   )
+ }
 
 export default Menu
