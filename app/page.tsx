e5c80fad4e68e0805a99d7bc23cc53f4b80a8eef
Matthew Powell <matt.powell2784@gmail.com>
2025-01-23 19:58:47 +0000

    feat: add home page text and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,42 @@
 import Link from 'next/link'
+import curryRiceImage from '@/app/assets/foodImages/curry_rice_bg.jpg'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center min-w-[320px]">
-      <button>
-        <Link href="/pages/login">Goto Login Page</Link>
-      </button>
+    <main className="relative w-screen min-w-[280px]">
+      <div className="absolute w-screen h-full z-0 ">
+        <Image
+          src={curryRiceImage}
+          fill
+          style={{ objectFit: 'cover' }}
+          alt="Curry and Rice on a table"
+          quality={80}
+        />
+      </div>
 
-      <button>
-        <Link href="/pages/logout">Goto Logout Page</Link>
-      </button>
+      <article className="relative min-h-[550px] md:min-h[650px] lg:min-h-[800px] h-screen lg:w-1/2 flex flex-col items-center lg:items-start justify-center lg:ml-16">
+        <div className="w-5/6 text-center lg:text-left ">
+          <h1 className="text-3xl md:text-4xl font-black text-twWhite mb-3 md:mb-5 ">
+            London&apos;s Luxurious Curry Club
+          </h1>
+          <p className="text-lg md:text-xl leading-tight md:leading-normal text-justify text-twWhite">
+            Indulge in authentic, exquisite flavours at our luxurious Curry Club
+            takeaway in London. Our expert chefs use only the freshest
+            ingredients and traditional cooking methods to create a menu of
+            classic and adventurous dishes that will transport you straight to
+            India. Place your order now and experience the ultimate luxury
+            takeaway.
+          </p>
+        </div>
 
-      <button>
-        <Link href="/pages/foodmenu">Goto Menu Page</Link>
-      </button>
+        <Link
+          className="bg-twPink text-xl rounded-lg p-4 mt-10 text-twWhite"
+          href={'/pages/foodmenu'}
+        >
+          View Food Menu
+        </Link>
+      </article>
     </main>
   )
 }
