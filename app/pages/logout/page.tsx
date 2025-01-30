3bb688487f4009609b1644ddb393329630972010
Matthew Powell <matt.powell2784@gmail.com>
2025-01-30 20:06:03 +0000

    style: style logout page

--- app/pages/logout/page.tsx
+++ app/pages/logout/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { signOut } from 'next-auth/react'
+import Image from 'next/image'
+import loginIcon from '@/app/assets/icons/login.png'
 
 const Logout = () => {
   const onSignOut = async () => {
@@ -8,13 +10,32 @@ const Logout = () => {
   }
 
   return (
-    <div className="flex flex-col items-center">
-      <p>Logout Page</p>
+    <section className="w-full flex flex-col items-center bg-twLightGrey min-h-screen">
+      <form
+        className="flexCol w-full max-w-[700px] md:rounded-3xl md:border-2 md:border-twPink px-4 md:mt-8 text-twBlack"
+        onSubmit={(e) => {
+          e.preventDefault()
+          onSignOut()
+        }}
+      >
+        <Image
+          src={loginIcon}
+          width={75}
+          height={75}
+          alt="Logout icon"
+          className="m-4 mt-8"
+        />
 
-      <button className="p-2 bg-red-500" onClick={onSignOut} type="submit">
-        Sign out
-      </button>
-    </div>
+        <p className="font-bold text-3xl text-twBlack">LOGOUT</p>
+
+        <button
+          type="submit"
+          className="flexRow gap-4 h-[50px] w-[300px] text-white text-lg font-bold bg-twBlack my-10 rounded active:bg-black/75"
+        >
+          Sign out
+        </button>
+      </form>
+    </section>
   )
 }
 
